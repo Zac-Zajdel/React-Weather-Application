9d87cf1fea16aa6b734924462bb51e018c483d81
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2018-12-23 01:21:37 -0500

    API is up and running

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Titles from './Components/Titles';
+import Form from './Components/Form';
+import Weather from './Components/Weather';
+
+const API_KEY = "d8bd6b7e4f8aa64693594808e8cb0819";
 
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div>
-        <p>Hello!</p>
-        <h1>YO!</h1>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather />
       </div>
     );
   }
